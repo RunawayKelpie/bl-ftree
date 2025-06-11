b675a2eb12f300e03dcb4a5b2b7cbe9db93eb180
RunawayKelpie <runawaykelpie@gmail.com>
2025-06-11 20:00:35 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,192 @@
-
 const rawData = {
-  nodes: [
-    { id: "Mister Coffee", label: "Mister Coffee", image: "https://via.placeholder.com/100?text=Mister+Coffee", shape: "image" },
-    { id: "Beacons", label: "Beacons", image: "https://via.placeholder.com/100?text=Beacons", shape: "image" },
-    { id: "My Trainwreck of a Life", label: "My Trainwreck of a Life", image: "https://via.placeholder.com/100?text=Trainwreck", shape: "image" },
-    { id: "Allan Hessey Big Band", label: "Allan Hessey Big Band", image: "https://via.placeholder.com/100?text=Hessey", shape: "image" },
-    { id: "Area 7", label: "Area 7", image: "https://via.placeholder.com/100?text=Area+7", shape: "image" },
-    { id: "Fiercebeard", label: "Fiercebeard", image: "https://via.placeholder.com/100?text=Fiercebeard", shape: "image" }
+  "nodes": [
+    {
+      "id": "Mister Coffee",
+      "label": "Mister Coffee",
+      "image": "https://via.placeholder.com/100?text=Mister+Coffee",
+      "shape": "image"
+    },
+    {
+      "id": "Beacons",
+      "label": "Beacons",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/kjH6wKDVRERh6apk_fCpUA/pzmSbVgFq14Gc8O4ypeP5vQGy-k-b7J0O_lyFVJfG_DeiSd7B8F_H_fFj4TDjxG3CVAymro9xLpSr2dfh7HmbOLU0K8zp_KRkJ5h58-c13oAAZyJzkUy9yj04g0hQ3hXWW60YBYP6OqYLgooCzldPg1_-KkYpuEbWVlsV73r9ug/gggHIg4KoeQ9kW28By6fU-hnw-NP0Lj-2uOoWEX9MmA",
+      "shape": "image"
+    },
+    {
+      "id": "My Trainwreck of a Life",
+      "label": "My Trainwreck of a Life",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/TdBMAyDysIcGvFaF_oO78w/FebfBrG1CdpN6VQ3JGdYxAUcfZ4W9FBA6zqAJ4ohpmznp-tZVi_3dPsbiE3ERSKWrP7r43vTfqBd-VN2Auf6Y16URYIdXaiD-I1vzm_hJBsemdI-NGAkJsiYKGo90P0UogkAUsTruAIYJhzHnuhQVMv_VQYFH-P3dFyvigIY5OR2bQ1APRWePyZp3ZDBGQHH/Xcfzguwy5Uh73d-B3mTYbnMgaMfnvkJlhrSHqEQYmlY",
+      "shape": "image"
+    },
+    {
+      "id": "Allan Hessey Big Band",
+      "label": "Allan Hessey Big Band",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/nzrOdnPk8AgP7eQqDMJ8Ig/Ikd62IxCdYrqpGFs16guP-9Cbe3YwWFhR0N-FB8eXONC6bBLP3ohLS5JSGemHfswoT39cGj4dhSmNJABNYYWbhpz72ENsBOUpAk_YRYlzcMru6qvyu7KW74mgo5aVs86qkMJHtnEzb4Dsj3KOCeuIVcVvEWAbr3x98vtElHUaM_zlpj-YZbYfflHrIucQkwF/R3hAFSGP-K2w3zhg5tseOtTygQjkuJ0NGP1PDWRq_FI",
+      "shape": "image"
+    },
+    {
+      "id": "Area 7",
+      "label": "Area 7",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/sOQpul61B74Rs7NqKWUgkA/dla7gQbEKliMjX2Y_srITdjy_aFnh52JIuRBADFK4PMRIE34DF-eZFkSunK1_-NEG-RqCNvNo9AbdiD-9wR6yLQMZmsHYuWlsAhqqWQ3VUkh1aetslmSz50N0LA-H9n2ibOQDRY_-UdK6bAzsIjRR0sjU0Ys7r60ld7FKjPCGO4/iQYAgOh1VTPX-usuyRBVYh2vuvoSdQd2YIviP0Nzzfc",
+      "shape": "image"
+    },
+    {
+      "id": "Fiercebeard",
+      "label": "Fiercebeard",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/W6AWNavg28wmzyNc8nISoA/DYa59unxvmnVISFceEY3IRDH1Ph1Y4YDclt-iBp2aLSCauC5-PRs-NXE76jiIow9HhRImzzCOEfZ5tvktslLRVCV25Vlv3upLKcDFdM5F_kK0hXGSDyP8yccsJRMxszE71s1EkR5pU8M0BbZWCygciz5mPLuyWJ3qL1l5g8aJqU/uYx8k8WgZb_JIbmh4iJe7MU19aAq0vvkwKyxIA-NQcw",
+      "shape": "image"
+    },
+    {
+      "id": "The Outsiders",
+      "label": "The Outsiders",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/qrR_aZ1GEsZmBGS41njgXA/UM-TTrt6cE_g2muq4PmdscMk3gX2_tN_NZ5E1PxyyJGksOGLjpAglZw2EkBiQ2fOeGW23J3GgUXASJK0WmlqpB-is2HTfxNdaEIPLmTISBaB7g4gHUln8giFvqCI5r2XT_PdUELMHQwLFhiDHDDAWSmqT-Ka5BpO3nOZ0csXXwo/FHdiDAJEExTcAdaxzBGwtosqxU0wP8w-Qio0PGvuPao",
+      "shape": "image"
+    },
+    {
+      "id": "Smartguy",
+      "label": "Smartguy",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/g5VZDIxY7ChaOQAtbYzgxw/G-8_gjlHTJEVj7sqXsQKQx2IJ_NbLkVKSxJHSHIDfb6H44BYP9W5lYlUEwimTQnc1WNifl01VMCxmPXVdQiAqKwSKgTa30GonKMy9cLW4zhcw8PlYVMXxA5wo5jBNbdg1cbO_uApBrAK_9rcVW2MXUoK4KDwnweucLiMQ6biuuM/_95bYM0n4Dn4SR-zpTbmIhuDL3kD7VrZT0IFM64rLeg",
+      "shape": "image"
+    },
+    {
+      "id": "Tarbash",
+      "label": "Tarbash",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/hNAW4Jo_EEd2SGNkeSWMkw/U-RRSVf9e0NZeYqEViLUUms_frfiZjRd22bV4wRaZsx2aLOf5NsYLJ_iQU82tEXa4v9-G2zIOg-MRLoFVtPJj7UNAh9Fjv0bLs7tPIxYl7FqmcLX5ygzD_zJnJ7sSN40tm2PCtSLzY_RSAhv0rno_wmYwCDtkBzT6IheKbNrG5U/uHGL8yjOXaviwdNBb7bVt5gnPCeesUbUoMPzAnxx4Dk",
+      "shape": "image"
+    },
+    {
+      "id": "Toga Rock",
+      "label": "Toga Rock",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/0GZ7OrpNOY-aDxPl8vhYkg/DsLvi0ITyOgdb61V-m6hJWs9CTDkNim7vhZPUFgqO5xCSBfNvQuV8mVxh2Yb6Pi0a2KVzXy4OGJkWfIBKNi8nPInQ4z9rTG9fEvYvzB_x6Hn-2lARa-F3TO2JPHeolSBxbasPVdWKfuT2NkVmb6sc4_NkZZpWAlYy7KetgZdZQM/kyY-bCmkiNRKtzD9c1TYDzazbCYaSbS9GEjHLgfgxqQ",
+      "shape": "image"
+    },
+    {
+      "id": "Wazska!",
+      "label": "Wazska!",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/EGpBFeiVRlnfFymPPJkhEQ/xa0cvL_TSOVRKgyj5m4tOAcrcQ06GVdM2K9s9Neace1F0L02FWXLtOpOjPruAWgvTAqARlEJ3XMMwJECQPzF6eYyx2qEj-1lOhlaQkIEGFKkJ0CoqyOye67dsmmi1QPPkWqOiiHaegvMU6KJuwEyvHKPK1km44yLKS8hpPxTbZM/ysRCfgB1fq8CDhbNoj0ofr27vzVdaNeasPHJmVf-Jv8",
+      "shape": "image"
+    },
+    {
+      "id": "12LB",
+      "label": "12LB",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/7x_H1GJ3HGn181MiMRDzuw/S2skKkrpy5Y7uRSZskcE4SPpSDsMVyI8_bLcNxr2cQxVjbSxe7BU0GErpuotygmI8hff8xXHf9SPKgJBV2-8f85FJMonApigrAL5qVKsy-y0o9tO3c1Zs2kW1ZSWr-yTLg44qOgf_U8BbzEAW8wHrA2r22OKKe37mvCbpM-ftbQ/1TBtpYnyhgt713Yt76oOIluAY-y9i0U3HUwHIUgEfhE",
+      "shape": "image"
+    },
+    {
+      "id": "Aspasia",
+      "label": "Aspasia",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/fdXZDOGKQNEsNi0nHG-LVA/nysDRtTLEPLkX5BXu8C7a8SHzPN03-ipwKqyUtdyMcC2YPdHt8m4RZWq7k23ZIyYuh0IlH5LzJJShFOlgOqU8-97ejbtqJ5hfs-kXNF7TK997rcqEcDNEnxTOOwNO3HjfI2Ct7JSC9oq36UwHt8oIVUlu1gjHTDczobbBtaT14Y/T4vVh_VIiEuCbDQr9jcueBRpW4WJMByDlCK41Yc7XWE",
+      "shape": "image"
+    },
+    {
+      "id": "Drew Money and the Dollars",
+      "label": "Drew Money and the Dollars",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/Pg-E1U6Fikvmb-GGjIzlGg/kKPZjzw7vRV1pYktP5i9gyxlQ33jA00wf2V0lpbzq_hQ4ooIQcPISv2Mz9k2DsU9v9pQW4aqQD6blm0MMPJts8n0pm4XDjwCKkIwOpyFE7TQXnYP1dKkdIh_9YeSR97C-CYuUzC9_PonGu71hSXaLPSp9LCPXntFRKq0-VDF7Me3Hi_ovEgisgXBL3YbC7lq/OyKujcB7VLY6iz7GUslY7UI7hBaD9J09z8S1c0uWpOk",
+      "shape": "image"
+    },
+    {
+      "id": "Harey",
+      "label": "Harey",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/bqq1_GwSUblDfjR0jEm-nQ/MUuPg-CmhYyQNyaR910lRw4IoKfRfnLylDgB0kdP_bMK5rUT7OD5ZA1BFKnYeCzjs8nzPREmzUHyh3im_ujlDec6Nvai3shtAlCpEQAnBS1fSjzdICnY1zpaDKA8o7qS7NHMuons5glQzxZLFq5aIi1IN_pu_OWJ1-YmAXex9ek/M4jnSzAa7afzGtQBarxpNRlPs95fyr5lfiY6j9PLCOU",
+      "shape": "image"
+    },
+    {
+      "id": "Midget Stooges",
+      "label": "Midget Stooges",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/fckQNlD3zqGUTQx0A2l-dg/TUWDR7yRnWupQQrJyvq4mAUFRPcCiXREhEQpSia1GDbXawu7F-f2Bg3BJLEV4Sb0AJ7Xg2g0ByR5MEVqWepZ262Pbrjttu2va3M6kCePjtsnp0P8DxABBtBu9tMr2ODpzVZGCsupQDZqSFXivL558aoLPpY1F5Y7cimfdvXz430/RgadQAYnsEQC-wzbpODLS9kuNXDnahA967bY_5hzhYs",
+      "shape": "image"
+    },
+    {
+      "id": "Oiska",
+      "label": "Oiska",
+      "image": "https://v5.airtableusercontent.com/v3/u/42/42/1749643200000/8_1SJB5N4viG1Pr-MS2cHg/SliagSnywKsR814RbmvccHvD3Hl3XB8I9dUEL5dTs3iEwJqp8Vm69POMiqrxVaYIX4ZHK37aSOr4wGKPGtnHC-B4E8ZTb1mLNqKNN1waOmIirhzuAp5gyA206psuWyhMmW-2geJPCPA1xzNPiS9EtR5uL-yXdTMtFQwENzC9DDQ/gIDDTahgQGuoUBXJxIgL2TtJrcba2I41ZoOH7rIvVIM",
+      "shape": "image"
+    }
   ],
-  edges: [
-    { from: "Mister Coffee", to: "Beacons", title: "Jimma Hobson, Tommy Gaffney, D. 'Ocker' O’Connell, Miranda ‘Diz’ De’Ath, Dougie Rankin, Andy Gardiner" },
-    { from: "Mister Coffee", to: "My Trainwreck of a Life", title: "Jimma Hobson, D. 'Ocker' O’Connell" },
-    { from: "Mister Coffee", to: "Allan Hessey Big Band", title: "D. 'Ocker' O’Connell" },
-    { from: "Mister Coffee", to: "Area 7", title: "D. 'Ocker' O’Connell, Andy Gardiner" },
-    { from: "Mister Coffee", to: "Fiercebeard", title: "D. 'Ocker' O’Connell, Andy Gardiner" },
-    { from: "Area 7", to: "Beacons", title: "Andy Gardiner, D. 'Ocker' O’Connell" },
-    { from: "Area 7", to: "My Trainwreck of a Life", title: "D. 'Ocker' O’Connell" },
-    { from: "Area 7", to: "Allan Hessey Big Band", title: "D. 'Ocker' O’Connell" },
-    { from: "Area 7", to: "Fiercebeard", title: "Andy Gardiner" },
-    { from: "Beacons", to: "My Trainwreck of a Life", title: "Jimma Hobson" },
-    { from: "Beacons", to: "Allan Hessey Big Band", title: "D. 'Ocker' O’Connell" },
-    { from: "Beacons", to: "Fiercebeard", title: "Andy Gardiner" },
-    { from: "My Trainwreck of a Life", to: "Allan Hessey Big Band", title: "D. 'Ocker' O’Connell" },
-    { from: "My Trainwreck of a Life", to: "Fiercebeard", title: "D. 'Ocker' O’Connell" },
-    { from: "Allan Hessey Big Band", to: "Fiercebeard", title: "D. 'Ocker' O’Connell" }
+  "edges": [
+    {
+      "from": "Mister Coffee",
+      "to": "Beacons",
+      "title": "Related by: Jimma Hobson, Tommy Gaffney, D. 'Ocker' O\u2019Connell, Miranda \u2018Diz\u2019 De\u2019Ath, Dougie Rankin, Andy Gardiner"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "My Trainwreck of a Life",
+      "title": "Related by: Jimma Hobson, D. 'Ocker' O\u2019Connell"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Allan Hessey Big Band",
+      "title": "Related by: D. 'Ocker' O\u2019Connell"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Area 7",
+      "title": "Related by: D. 'Ocker' O\u2019Connell, Andy Gardiner"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Fiercebeard",
+      "title": "Related by: D. 'Ocker' O\u2019Connell, Andy Gardiner"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "The Outsiders",
+      "title": "Related by: D. 'Ocker' O\u2019Connell, Andy Gardiner"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Smartguy",
+      "title": "Related by: D. 'Ocker' O\u2019Connell, Monkee Liddell"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Tarbash",
+      "title": "Related by: D. 'Ocker' O\u2019Connell, Monkee Liddell, Timmy von Carate"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Toga Rock",
+      "title": "Related by: D. 'Ocker' O\u2019Connell"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Wazska!",
+      "title": "Related by: D. 'Ocker' O\u2019Connell"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "12LB",
+      "title": "Related by: Timmy von Carate"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Aspasia",
+      "title": "Related by: Miranda \u2018Diz\u2019 De\u2019Ath"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Drew Money and the Dollars",
+      "title": "Related by: Miranda \u2018Diz\u2019 De\u2019Ath"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Harey",
+      "title": "Related by: Miranda \u2018Diz\u2019 De\u2019Ath"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Midget Stooges",
+      "title": "Related by: Miranda \u2018Diz\u2019 De\u2019Ath"
+    },
+    {
+      "from": "Mister Coffee",
+      "to": "Oiska",
+      "title": "Related by: Miranda \u2018Diz\u2019 De\u2019Ath"
+    }
   ]
 };
+
 
 function drawGraph() {
   const seed = document.getElementById('seedInput').value.trim();
@@ -54,7 +215,9 @@ function drawGraph() {
 
     const node = rawData.nodes.find(n => n.id === current.id);
     if (node) {
-      if (current.level === 0) node.level = 0;
+      if (current.level === 0) {
+        node.x = 0; node.y = 0; node.fixed = true;
+      }
       nodes.push(node);
     }
 
@@ -80,28 +243,37 @@ function drawGraph() {
     edges: new vis.DataSet(edges)
   };
   const options = {
-    layout: {
-      hierarchical: {
-        enabled: true,
-        direction: "UD",
-        sortMethod: "directed"
+    physics: {
+      enabled: true,
+      solver: "repulsion",
+      repulsion: {
+        nodeDistance: 300,
+        springLength: 400,
+        springConstant: 0.04
+      },
+      stabilization: {
+        iterations: 100
       }
-    },
-    edges: {
-      arrows: "to",
-      color: "gray",
-      font: { size: 10 },
-      smooth: false
     },
     nodes: {
       shape: "image",
       size: 60,
       font: { size: 14, color: "#000", strokeWidth: 2, strokeColor: "#fff" }
     },
+    edges: {
+      arrows: false,
+      color: "gray",
+      font: { size: 10 },
+      smooth: {
+        type: "dynamic"
+      }
+    },
     interaction: {
       hover: true
     },
-    physics: false
+    layout: {
+      improvedLayout: true
+    }
   };
   new vis.Network(container, data, options);
 }
